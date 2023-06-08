99ed9350f08687570281f5fc34a9ad442e26e43e
manormasharma05 <manorma@hackerearth.com>
2023-06-09 02:03:12 +0530

    made change

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import Header from './components/Header';
 import Home from './Pages/Homepage';
 import Footer from './components/Footer';
 import Resume from './Pages/Resume/resume';
-import { HashRouter as Router, Route } from 'react-router-dom';
-
+import {
+  Route,Routes
+} from 'react-router-dom'
 function App() {
   return (
     <>
-      <Router>
         <Header />
           <div className='main'>
-            <Route index element={<Home />} />
-            <Route path="resume" element={<Resume />} />
+            <Routes>
+                <Route index element={<Home />} />
+                <Route path="/resume" element={<Resume />} />
+            </Routes>
           </div>
         <Footer />
-      </Router>
     </>
   );
 }
